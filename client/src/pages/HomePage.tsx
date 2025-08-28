9f811ae17e70bc8d7b33d6ebce2b5cec0af12867
= <virajkc1@gmail.com>
2025-08-29 00:31:21 +0100

    Landing page development

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
-import { ChevronRight, GraduationCap } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import HomePage_1 from "../assets/homepage_icons/homepage_stack1.png";
+import HomePage_2 from "../assets/homepage_icons/homepage_stack2.png";
+import HomePage_3 from "../assets/homepage_icons/homepage_stack3.png";
 
 const HomePage = () => {
   const { scrollY } = useScroll();
 
   // Central text animation
-  const textOpacity = useTransform(scrollY, [0, 400], [1, 0]);
-  const textY = useTransform(scrollY, [0, 10], [0, -50]);
+  // const textOpacity = useTransform(scrollY, [0, 400], [1, 0]);
+  // const textY = useTransform(scrollY, [0, 10], [0, -50]);
 
   // Top boxes move slower, subtle rotation
-  const rotateLeftTop = useTransform(scrollY, [0, 800], [5, 0]);
 
-  const yRightTop = useTransform(scrollY, [0, 800], [0, -200]);
-  const rotateRightTop = useTransform(scrollY, [0, 800], [-5, 0]);
+  const yLeftTop = useTransform(scrollY, [0, 800], [0, -200]);
+
+  const yLeftBottom = useTransform(scrollY, [0, 800], [0, 200]);
 
   return (
     <div className=" bg-white">
@@ -72,88 +74,114 @@ const HomePage = () => {
         </div>
       </nav>
       {/* Hero Section */}
-      <section className=" min-h-screen bg-cover bg-center gradient-vertical bg-white text-black">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
-          <div className="flex flex-col gap-12 items-center h-96">
-            <div className="m-6">
-              {/* Left Side */}
-              <motion.div
-                style={{ rotate: rotateLeftTop }}
-                className="bg-slate-500 absolute  right-32 w-52 h-52 rounded-xl z-10 shadow-xl "
-              ></motion.div>
+      <section className=" min-h-[200vh] bg-cover bg-center gradient-vertical bg-white text-black relative">
+        <div className="sticky top-0 z-30 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
+          <h1 className="text-7xl pt-10 font-normal leading-tight text-black">
+            <span className=" block text-left  ml-40">Build a future</span>
 
-              {/* Right Side */}
+            <span className="block text-right green-primary-text mb-5 mr-40">
+              ... in Unisun
+            </span>
+          </h1>
+          <div className="max-w-screen-sm mx-auto">
+            <p className="text-lg text-gray-700 leading-relaxed text-center mb-5  ">
+              Be part of a community of students and alumni to help you through
+              your academic journey
+            </p>
+          </div>
 
-              {/* Central Text */}
-              <motion.div
-                style={{ opacity: textOpacity, y: textY }}
-                className="items-center justify-center"
-              >
-                <h1 className="text-7xl pt-10 font-normal leading-tight text-center  text-black">
-                  <span className="text-black block text-left -mb-12 ml-10">
-                    Build a future
-                  </span>
-                  <br className="mt-0" />
-                  <span className="text-black block text-right -mt-10 mr-12 green-primary-text">
-                    ... in Unisun
-                  </span>
-                </h1>
-                <div className="max-w-screen-sm mx-auto">
-                  <p className="text-lg text-gray-700 mt-4 mb-8 leading-relaxed text-center ">
-                    Be part of a community of students and alumni to help you
-                    through your academic journey
-                  </p>
-                </div>
-              </motion.div>
-              <div className="flex justify-center">
-                <Link
-                  to="/signup"
-                  className="btn-primary-color inline-block px-6 py-3 rounded-lg text-center whitespace-nowrap w-auto hover:scale-105 hover:shadow-xl"
-                >
-                  Join the Community
-                </Link>
-              </div>
-            </div>
+          <div className="flex justify-center">
+            <Link
+              to="/signup"
+              className="btn-primary-color px-6 py-3 rounded-lg text-center whitespace-nowrap  hover:scale-105 hover:shadow-xl"
+            >
+              Join the Community
+            </Link>
           </div>
         </div>
+        <motion.div
+          className="absolute top-[30%] left-[5%] w-[450px] z-10 h-[300px] bg-red-500 rounded-lg"
+          style={{
+            y: yLeftTop,
+            willChange: "transform",
+            backgroundImage: `url(${HomePage_3})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></motion.div>
+        <motion.div
+          className="absolute top-[40%] left-[15%] w-[450px] h-[300px] rounded-lg overflow-hidden z-20 "
+          style={{
+            y: yLeftBottom,
+            willChange: "transform",
+            backgroundImage: `url(${HomePage_2})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></motion.div>
+        <motion.div
+          className="absolute top-[25%] right-[5%] w-[450px] z-10 h-[300px] bg-red-500 rounded-lg"
+          style={{
+            y: yLeftBottom,
+            willChange: "transform",
+            backgroundImage: `url(${HomePage_1})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></motion.div>
       </section>
       {/* </div>
 
       {/* Features Section */}
-      <section className="py-16 bg-gray-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
-            <div>
-              <h2 className="text-3xl font-medium text-gray-900 mb-6">
-                How it works
-              </h2>
-              <p className="text-lg text-gray-600 mb-6">
-                Access free resources, connect with peers, and build your skills
-                with real-world projects. Get support from the community and
-                learn from experts in your field.
+      <section className="mt-10 pt-16 bg-white border-t-2 border-gray-100">
+        <div className="max-w-7xl mb-0  mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-5xl font-normal text-center pb-10 text-gray-900 mb-12">
+            How it works
+          </h2>
+          <div className="flex justify-center flex-wrap gap-16 ">
+            <div className="bg-[#cbe2cb] rounded-2xl shadow-md w-[360px] h-[400px] p-6 flex flex-col items-center text-center">
+              <div className="px-4 mb-2">
+                <h3 className="text-xl font-medium text-black flex justify-between items-center">
+                  <span>Create an account</span>
+                </h3>
+              </div>
+              <p className="text-gray-600">
+                This is a short description of feature 1.
               </p>
-              <Link
-                to="/students"
-                className="text-blue-600 hover:text-blue-800 font-semibold inline-flex items-center"
-              >
-                Learn more <ChevronRight className="ml-1 h-4 w-4" />
-              </Link>
+              <img
+                src="https://via.placeholder.com/150"
+                alt="Feature 1"
+                className="mb-4 rounded-lg"
+              />
             </div>
-            <div className="relative">
-              <motion.div
-                style={{ y: yRightTop, rotate: rotateRightTop }}
-                className=" pt-10 rounded-xl z-10 shadow-xl"
-              >
-                <img
-                  src={HomePage_1}
-                  alt="Floating"
-                  className="w-full h-full object-cover rounded-xl"
-                />
-              </motion.div>
+
+            <div className="bg-[#cbe2cb] rounded-2xl shadow-lg w-[360px] h-[400px] p-6 flex flex-col items-center text-center">
+              <h3 className="text-xl font-medium mb-2">Ask Questions</h3>
+              <p className="text-gray-600">
+                This is a short description of feature 1.
+              </p>
+              <img
+                src="https://via.placeholder.com/150"
+                alt="Feature 1"
+                className="mb-4 rounded-lg"
+              />
+            </div>
+
+            <div className="bg-[#cbe2cb] rounded-2xl shadow-lg w-[360px] h-[400px] p-6 flex flex-col items-center text-center">
+              <h3 className="text-xl font-medium mb-2">Help Others</h3>
+              <p className="text-gray-600">
+                This is a short description of feature 1.
+              </p>
+              <img
+                src="https://via.placeholder.com/150"
+                alt="Feature 1"
+                className="mb-4 rounded-lg"
+              />
             </div>
           </div>
         </div>
       </section>
+
       <section className="py-16 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center"></div>
@@ -187,6 +215,11 @@ const HomePage = () => {
           </div>
         </div>
       </section>
+      <footer className="py-16 bg-blue-500">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="grid gap-16 items-center"></div>
+        </div>
+      </footer>
     </div>
   );
 };
